e1c6bbf29908227ee208e89247038562ebd0ba7d
Martin Nusspaumer <142355408+KlassicStudio@users.noreply.github.com>
2024-01-24 00:01:46 +0000

    feat (fg-33): add custom new account

--- src/front/js/component/addAccount.js
+++ src/front/js/component/addAccount.js
@@ -1,51 +1,122 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import { Row, Col } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import { Row, Col, Modal, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 export const AddAccount = () => {
   const { store, actions } = useContext(Context);
+  const [show, setShow] = useState(false);
+  const [accountType, setAccountType] = useState("");
+  const [balance, setBalance] = useState("");
 
-	useEffect(() => {
-		if(store.token && store.token!="" && store.token!=undefined)
-			 actions.getBalances();
-	}, [store.token])
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const colors = ['#DFEF6A', '#B8DBAD', '#c77593', '#CD554F'];
+  const handleAddAccount = async (event) => {
+    event.preventDefault();
+    try {
+      // Call the addNewAccount action with the accountType and balance
+      await actions.addNewAccount(accountType, balance);
+      // Fetch the updated list of accounts
+      await actions.getBalances();
+      // Close the modal
+      handleClose();
+    } catch (error) {
+      // Handle any errors here
+      console.error("Error adding new account", error);
+    }
+  };
+
+  useEffect(() => {
+    if (store.token && store.token != "" && store.token != undefined)
+      actions.getBalances();
+  }, [store.token]);
+
+  const colors = ["#DFEF6A", "#B8DBAD", "#c77593", "#CD554F"];
 
   return (
-
     <>
-    <div className="center-item-container">
-      <div className="right-items">
-        <h5 className="mb-3">My Accounts</h5>
-        <Button variant="outline-success" size="sm" className="new-account-btn">
-          Create Account
-        </Button>
-        <hr/>
-        {store.balances && (
-          <>
-            {Object.keys(store.balances).reduce((rows, acctName, index) => {
-              if (index % 3 === 0) {
-                rows.push([]);
-              }
-              rows[rows.length - 1].push(
-                <Col sm={4} key={acctName} className="text-center">
-                  <div className="acctBadges" style={{backgroundColor: colors[index % colors.length]}}>
-                    <span><i className="fa-solid fa-wallet"></i> {acctName}</span>
-                  </div>
-                </Col>
-              );
-              return rows;
-            }, []).map((cols, index) => (
-              <Row key={index}>{cols}</Row>
-            ))}
-          </>
-        )}
+      <div className="center-item-container">
+        <div className="right-items">
+          <h5 className="mb-3">My Accounts</h5>
+          <Button
+            style={{ backgroundColor: "#85A47C" }}
+            size="sm"
+            className="new-account-btn"
+            onClick={handleShow}
+          >
+            <i className="fa-solid fa-circle-plus"></i> New Account
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>New Account</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form onSubmit={handleAddAccount}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Account Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter account name"
+                    value={accountType}
+                    onChange={(e) => setAccountType(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Balance</Form.Label>
+                  <Form.Control
+                    type="text"
+                    pattern="^\d*(\.\d{0,2})?$"
+                    placeholder="Enter balance"
+                    value={balance}
+                    onChange={(e) => setBalance(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Button variant="success" type="submit">
+                  Create New Account
+                </Button>
+              </Form>
+            </Modal.Body>
+          </Modal>
+
+          <hr />
+          {store.balances && (
+            <>
+              {Object.keys(store.balances)
+                .reduce((rows, acctName, index) => {
+                  if (index % 3 === 0) {
+                    rows.push([]);
+                  }
+                  rows[rows.length - 1].push(
+                    <Col sm={4} key={acctName} className="text-center">
+                      <div
+                        className="acctBadges"
+                        style={{
+                          backgroundColor: colors[index % colors.length],
+                        }}
+                      >
+                        <span>
+                          <i className="fa-solid fa-wallet"></i> {acctName}
+                        </span>
+                      </div>
+                    </Col>
+                  );
+                  return rows;
+                }, [])
+                .map((cols, index) => (
+                  <Row key={index}>{cols}</Row>
+                ))}
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </>
-);
-}
+    </>
+  );
+};
 
 export default AddAccount;
